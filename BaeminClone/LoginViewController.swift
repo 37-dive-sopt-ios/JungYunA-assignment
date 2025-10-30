923994504f3692bc3304f10826d41142db5ef77e
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-10-30 13:29:53 +0900

    fix: 프로젝트 이름 변경

---
+++ BaeminClone/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  BaminClone
+//
+//  Created by 정윤아 on 10/30/25.
+//
+
+import Foundation
+import UIKit
+//import Snapkit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+    
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let label = UILabel()
+        label.text = "Login"
+        label.textAlignment = .center}
+}
