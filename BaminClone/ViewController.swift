8c11e29f2ff6331e176c218113f3a57f6ddef02e
laura-jung <166522604+laura-jung@users.noreply.github.com>
2025-10-30 11:08:53 +0900

    initial commit

---
+++ BaminClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaminClone
+//
+//  Created by 정윤아 on 10/30/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
